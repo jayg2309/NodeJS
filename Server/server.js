f31eea9b2e53d1fbb112b4b99721104c74bf2ead
Jay <jaygondaliya2309@gmail.com>
2025-07-22 04:12:24 +0530

    Building nginx server using nodejs

---
+++ Server/server.js
@@ -0,0 +1,31 @@
+const http = require("http"); // http has alot of modules
+const fs = require("fs");
+const path = require("path");
+const { log } = require("console");
+
+const port = 3000;
+
+//create server
+// inside .createServer - give functionality of what you want it to do
+const server = http.createServer((req, res) => {
+  // request and response
+  const filePath = path.join(
+    __dirname,
+    req.url === "/" ? "index.html" : "req.url" // if true execute left else right of ?
+  );
+
+  const extensionName = String(path.extname(filePath)).toLowerCase;
+
+  //allowing what type of file supporting
+  const mimeType = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".png": "text/png",
+  };
+});
+
+//listen - 2 parameters - what port no, what should it do
+server.listen(port, () => {
+  console.log(`Server is listening on ${port}`);
+});
